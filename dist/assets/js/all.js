2e3852fcf5efc6b80d997426a46cd7baef8f0845
Diogo Moretti <diogo.pearljam@gmail.com>
2014-10-22 00:43:50 -0200

    add js for menu

--- dist/assets/js/all.js
+++ dist/assets/js/all.js
@@ -180,58 +180,109 @@ window.loadGoogleMaps = function () {
         map: map
     });
 };
-;// 21 Dez 2012, 4:42
-;(function ($, window, document, undefined) {
-	'use strict';
-	({
-		animateScroll : function () {
-			$(".main-menu").find('.main-menu-link').on('click', function (event) {
-
-				var $this = $(this),
-					$htmlBody = $('html, body'),
-					linkTarget = $this.attr('href'),
-					offSetTop;
-
-				// If not start with #, stop here!
-				if (linkTarget[0] !== '#') {
-					return false;
-				}
-
-				event.preventDefault();
-
-				// Get distance of top
-				offSetTop = $(linkTarget).offset().top;
-
-				// Animate the scroll
-				$htmlBody.stop().animate({scrollTop : offSetTop}, function () {
-					location.hash = linkTarget;
-				});
-			});
-		},
-
-		init : function () {
-			var that = this;
-
-			$(function () {
-				that.animateScroll();
-			});
-		}
-	}).init();
-}(jQuery, window, document));
-
-var mainMenu = $('.main-menu'),
-    mainMenuFixed = 'main-menu-fixed'
-    headerSize = $('.header').height()
-    bodyOffset = $(window).scrollTop();
+;
+!
+function () {
+    var id, target, current, previous
  
-$(window).scroll(function () {
-    if ($(this).scrollTop() > headerSize + 190) {
-        mainMenu.addClass(mainMenuFixed);
-    } else {
-        mainMenu.removeClass(mainMenuFixed);
+    function step() {
+        current = window.pageYOffset - (window.pageYOffset - target) / (target ? 5 : 2)
+ 
+        document.body.scrollTop = current
+        document.documentElement.scrollTop = current
+ 
+        if (previous != current) {
+            window.requestAnimationFrame(step)
+        } else {
+            document.location.hash = id
+            delete step.runnig
+        }
+        previous = current
     }
-});
+ 
+    function handler(event) {
+        event.preventDefault()
+        id = this.getAttribute('href')
+        if (id == '#') {
+            target = 0;
+        } else {
+            target = document.querySelector(id).getBoundingClientRect().top + window.pageYOffset
+        }
+ 
+        if (!step.runnig) {
+            previous = null
+            step.runnig = true
+            step()
+        }
+    }
+ 
+    var links = document.querySelectorAll('a[href*="#"]');
+    [].slice.call(links).forEach(function (link) {
+        link.addEventListener('click', handler)
+    })
+}();
 
-if (bodyOffset > headerSize + 190) {
-    mainMenu.addClass(mainMenuFixed);
-}
+!function () {
+  function ScrollSpy() {
+    this.scrollLast = 0
+    this.nav = document.querySelector('[data-nav]')
+    this.active = null
+  }
+
+  ScrollSpy.prototype.init = function () {
+    this.addEventListeners()
+    this.updateSections()
+  }
+
+  ScrollSpy.prototype.updateSections = function () {
+    var elements = document.querySelectorAll('header, section')
+    this.sections = this.getSections(elements)
+    this.nav_offset = this.nav.getBoundingClientRect().top + window.pageYOffset;
+    this.menu()
+  }
+
+  ScrollSpy.prototype.getSections = function (elements) {
+    return [].slice.call(elements).map(function (element) {
+      return {
+        id: element.id
+      , offset: element.getBoundingClientRect().top + window.pageYOffset - 20
+      }
+    })
+  }
+
+  ScrollSpy.prototype.getActive = function () {
+    var scroll = window.pageYOffset
+      , section
+
+    for (var i = 0, l = this.sections.length; i < l; i++) {
+      this.sections[i].offset <= scroll && (section = this.sections[i])
+    }
+    return section.id
+  }
+
+  ScrollSpy.prototype.addEventListeners = function () {
+    document.addEventListener('scroll', this.onScroll.bind(this))
+    document.addEventListener('DOMContentLoaded', this.updateSections.bind(this))
+    document.addEventListener('resize', this.updateSections.bind(this))
+    window.addEventListener('load', this.updateSections.bind(this))
+  }
+
+  ScrollSpy.prototype.onScroll = function (event) {
+    if ((Date.now() - this.scrollLast) > 50) {
+      this.scrollLast = Date.now()
+      this.menu()
+    }
+  }
+
+  ScrollSpy.prototype.menu = function () {
+    var id = this.getActive()
+      , item = this.nav.querySelector('a[href="#' + id + '"]')
+
+    this.active && this.active.classList.remove('active')
+    item && item.classList.add('active')
+    this.nav.classList[this.nav_offset < window.pageYOffset ? 'add' : 'remove']('main-menu-fixed')
+    this.active = item
+  }
+
+  ;(new ScrollSpy).init()
+}();
